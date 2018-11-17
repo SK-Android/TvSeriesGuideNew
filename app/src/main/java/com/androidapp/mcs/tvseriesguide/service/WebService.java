7200f72245b91f0b63a608829b0703c6c6dd73b5
Saudamini Keshav <srjoshi279@gmail.com>
2018-11-16 18:46:14 -0600

    .

--- app/src/main/java/com/androidapp/mcs/tvseriesguide/service/WebService.java
+++ app/src/main/java/com/androidapp/mcs/tvseriesguide/service/WebService.java
@@ -15,11 +15,6 @@ public interface WebService {
     String FEEDSERIES = "?apikey=2b0cf4b2&s=game&type=series";
     String FEED = "?apikey=2b0cf4b2";
 
-
-//&i=tt0944947 game of thrones id
-//    www.omdbapi.com/?apikey=2b0cf4b2&i=tt0944947&season=1
-
-
     Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build();
@@ -27,11 +22,6 @@ public interface WebService {
     //Call to get a list of series
     @GET(FEEDSERIES)
     Call<Series> getSeries();
-
-//
-//    @GET("/?apikey=" + API_KEY)
-//    Call<EpisodeResponse> getEpisodes(@Query("i") String id, @Query("SEASON") String SEASON);
-
 
     @GET(FEED)
     Call<Episode> getEpisodes(
@@ -44,14 +34,4 @@ public interface WebService {
     Call<Details> getDetails(
             @Query("i") String id, @Query("season") String season, @Query("episode") String episode);
     //http://www.omdbapi.com/?apikey=2b0cf4b2&i=tt0944947&i=tt0944947&season=1&episode=1
-
-//    @GET(FEED)
-//    Call<Season> getSeasons(@Query("IMDBID") String IMDBID);
-//http://www.omdbapi.com/?apikey=2b0cf4b2&i=tt0944947
-    //    @GET("?apikey=2b0cf4b2&season=1")
-//    Call<Episode> getSeasonCount(@Query("IMDBID") String IMDBID);
-//
-    //Call to get a list of episodes with a value of Total Seasons
-//    @GET("?apikey=2b0cf4b2")
-//    Call<Episode> getEpisodes(@Query("IMDBID") String IMDBID, @Query("SEASON") String SEASON);
 }
