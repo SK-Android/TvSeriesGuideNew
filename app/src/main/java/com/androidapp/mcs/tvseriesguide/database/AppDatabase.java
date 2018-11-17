265dd02fa825460b3383911a9bfa086156d08711
Saudamini Keshav <srjoshi279@gmail.com>
2018-11-16 18:37:43 -0600

    .

--- app/src/main/java/com/androidapp/mcs/tvseriesguide/database/AppDatabase.java
+++ app/src/main/java/com/androidapp/mcs/tvseriesguide/database/AppDatabase.java
@@ -16,17 +16,21 @@ import com.androidapp.mcs.tvseriesguide.model.Series;
 //that Room uses to generate code in the background. I'll create an implementing class that's what will be used at runtime
 //This is a Singleton class
 
-@Database(entities = {Series.class, Seasons.class, Rating.class, EpisodeResponse.class, Episode.class, Details.class}, version = 1) //entities -> comma delimited list of entity classes
+@Database(entities = {Series.class, Seasons.class, Rating.class, EpisodeResponse.class, Episode.class, Details.class}, version = 1)
+//entities -> comma delimited list of entity classes
 public abstract class AppDatabase extends RoomDatabase {                                                                            //version = 1 -> First version of database structure
 
     public static final String DATABASE_NAME = "AppDatabase.db";                             //Database name useful when testing (Not actually required)
     private static volatile AppDatabase instance;                                            //This is a singleton of the database. Votalatile so that it can only be referenced from Main memory
     private static final Object LOCK = new Object();                                         //Object to synchronize on
 
-                                                                                             //For each Database Access object create an abstract method
+    //For each Database Access object create an abstract method
     public abstract SeriesDao seriesDao();                                                   //This will return an instance of the Dao Interface
+
     public abstract SeasonsDao seasonsDao();
+
     public abstract EpisodeResponseDao episodeResponseDao();
+
     public abstract RatingDao ratingDao();
 
 
@@ -34,12 +38,13 @@ public abstract class AppDatabase extends RoomDatabase {                        
         if (instance == null) {
             synchronized (LOCK) {                                                               //Synchronise on lock object so that two parts of the application dont try to create database at the same time
                 if (instance == null) {                                                         //Secondary check
-                instance = Room.databaseBuilder(context.getApplicationContext()                  //Use builder object that's available in the Room class
-                        , AppDatabase.class, "app-database")                               // Should always use application context
-                        .allowMainThreadQueries()
-                        .build();                                                                // Next argument is a class that needs to be instantiated to define database itself
-                                                                                                 //3rd argument -> Name of the database as it will be stored in the persistent storage
-            }}
+                    instance = Room.databaseBuilder(context.getApplicationContext()                  //Use builder object that's available in the Room class
+                            , AppDatabase.class, "app-database")                               // Should always use application context
+                            .allowMainThreadQueries()
+                            .build();                                                                // Next argument is a class that needs to be instantiated to define database itself
+                    //3rd argument -> Name of the database as it will be stored in the persistent storage
+                }
+            }
 
         }
         return instance;
