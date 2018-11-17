8dde5e7046472638f8d1a599e0913a7e5a46ce81
Saudamini Keshav <srjoshi279@gmail.com>
2018-11-16 18:38:31 -0600

    .

--- app/src/main/java/com/androidapp/mcs/tvseriesguide/utils/MyNetworkHelper.java
+++ app/src/main/java/com/androidapp/mcs/tvseriesguide/utils/MyNetworkHelper.java
@@ -5,16 +5,15 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 public class MyNetworkHelper {
-   public static boolean hasNetworkStatus(Context context)
-   {
+    public static boolean hasNetworkStatus(Context context) {
 
-       try {
-           ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);       //To check connectivity use a class named Connectivity manager
-           NetworkInfo  activeNetwork = cm.getActiveNetworkInfo();                                                  // Its part of the package android.net. It returns an object
-           return activeNetwork != null && activeNetwork.isConnectedOrConnecting();                                                                                    //Cast it to return connectivity manager
-       } catch (Exception e) {
-           e.printStackTrace();
-           return false;
-       }
-   }
+        try {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);       //To check connectivity use a class named Connectivity manager
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();                                                  // Its part of the package android.net. It returns an object
+            return activeNetwork != null && activeNetwork.isConnectedOrConnecting();                                                                                    //Cast it to return connectivity manager
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
